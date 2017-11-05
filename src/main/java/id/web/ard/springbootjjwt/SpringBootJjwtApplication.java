607978d816781a05127e18fb0a07623b6b023a01
ard333 <ard333.ardiansyah@gmail.com>
2017-11-05 10:35:38 +0700

    project setup

---
+++ src/main/java/id/web/ard/springbootjjwt/SpringBootJjwtApplication.java
@@ -0,0 +1,12 @@
+package id.web.ard.springbootjjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJjwtApplication.class, args);
+	}
+}
